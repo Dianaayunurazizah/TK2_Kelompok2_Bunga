48f5265a1f8c595cd89f17601c04ff9593f88c9e
EnigmaFaceless <ekajuara97@gmail.com>
2026-09-21 17:54:03 +0700

    add Class Mawar

---
+++ src/bunga/Mawar.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bunga;
+
+/**
+ *
+ * @author Eka Melawan ERROR
+ */
+public class Mawar {
+    
+    String warna;
+    String pertulanganDaun;
+    String jenisBatang;
+    String aroma;
+    String asal;
+
+    public Mawar(String warna, String pertulanganDaun, String jenisBatang, String aroma, String asal) {
+        this.warna = warna;
+        this.pertulanganDaun = pertulanganDaun;
+        this.jenisBatang = jenisBatang;
+        this.aroma = aroma;
+        this.asal = asal;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public void setPertulanganDaun(String pertulanganDaun) {
+        this.pertulanganDaun = pertulanganDaun;
+    }
+
+    public void setJenisBatang(String jenisBatang) {
+        this.jenisBatang = jenisBatang;
+    }
+
+    public void setAroma(String aroma) {
+        this.aroma = aroma;
+    }
+
+    public void setAsal(String asal) {
+        this.asal = asal;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public String getPertulanganDaun() {
+        return pertulanganDaun;
+    }
+
+    public String getJenisBatang() {
+        return jenisBatang;
+    }
+
+    public String getAroma() {
+        return aroma;
+    }
+
+    public String getAsal() {
+        return asal;
+    }
+    
+    
+}
